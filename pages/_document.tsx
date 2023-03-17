887cb4d6fc3b5a799a48aecf82a16e06c52f2ae1
Othman Shareef <othmanosx@gmail.com>
2023-03-17 21:35:04 +0300

    Feat: add og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Brieflytics" />
+        <meta property="og:image" content="/thumbnail.webp" />
+        <meta
+          property="og:description"
+          content="streamline project management with AI-powered daily summaries from Jira, Github, and more"
+        />
+        <meta property="og:url" content="https://brieflytics.vercel.app/" />
+      </Head>
       <body>
         <Main />
         <NextScript />
